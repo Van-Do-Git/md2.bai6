541744f46ac91d02abdf570771fdb7455cfecbf2
unknown <mittervan@gmail.com>
2021-07-12 17:15:32 +0700

    Exercise of class Circle

--- src/PractiveShape/Circle.java
+++ src/PractiveShape/Circle.java
@@ -1,2 +1,51 @@
-package PractiveShape;public class Circle {
+package PractiveShape;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle() {
+        radius = 1.0;
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getPrimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A circle with radius = " + radius + " ," +
+                "which is a subclass of " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle);
+
+        circle = new Circle(3.5);
+        System.out.println(circle);
+
+        circle = new Circle(3.5, "indigo", false);
+        System.out.println(circle);
+    }
 }
--- src/PractiveShape/Rectangle.java
+++ src/PractiveShape/Rectangle.java
@@ -1,2 +1,65 @@
-package PractiveShape;public class Rectangle {
+package PractiveShape;
+
+public class Rectangle extends Shape {
+    private double width;
+    private double length;
+
+    public Rectangle() {
+        width = 1.0;
+        length = 1.0;
+    }
+
+    public Rectangle(double _width, double _length) {
+        width = _width;
+        length = _length;
+    }
+
+    public Rectangle(double _width, double _length, String _color, boolean _filled) {
+        super(_color, _filled);
+        width = _width;
+        length = _length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + length);
+    }
+
+    @Override
+    public String toString() {
+        return "A rectangle with width= " + width + " " +
+                "and leng= " + length + ", " +
+                "which is a subclass of " + super.toString();
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.3, 5.8);
+        System.out.println(rectangle);
+
+        rectangle = new Rectangle(2.5, 3.8, "orange", true);
+        System.out.println(rectangle);
+    }
+
 }
